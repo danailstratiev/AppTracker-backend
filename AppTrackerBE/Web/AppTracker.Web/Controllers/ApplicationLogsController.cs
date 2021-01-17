54421994bc9e950a14c65fce720809019f14d0f6
danailstratirv <stratyd@abv.bg>
2021-01-17 12:27:06 +0200

    Initial scaffold of the controllers

---
+++ AppTrackerBE/Web/AppTracker.Web/Controllers/ApplicationLogsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppTracker.Data;
+using AppTracker.Data.Models;
+
+namespace AppTracker.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationLogsController : ControllerBase
+    {
+        private readonly AppTrackerDbContext _context;
+
+        public ApplicationLogsController(AppTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApplicationLogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationLog>>> GetApplicationLogs()
+        {
+            return await _context.ApplicationLogs.ToListAsync();
+        }
+
+        // GET: api/ApplicationLogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationLog>> GetApplicationLog(int id)
+        {
+            var applicationLog = await _context.ApplicationLogs.FindAsync(id);
+
+            if (applicationLog == null)
+            {
+                return NotFound();
+            }
+
+            return applicationLog;
+        }
+
+        // PUT: api/ApplicationLogs/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationLog(int id, ApplicationLog applicationLog)
+        {
+            if (id != applicationLog.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(applicationLog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationLogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApplicationLogs
+        [HttpPost]
+        public async Task<ActionResult<ApplicationLog>> PostApplicationLog(ApplicationLog applicationLog)
+        {
+            _context.ApplicationLogs.Add(applicationLog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApplicationLog", new { id = applicationLog.Id }, applicationLog);
+        }
+
+        // DELETE: api/ApplicationLogs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationLog(int id)
+        {
+            var applicationLog = await _context.ApplicationLogs.FindAsync(id);
+            if (applicationLog == null)
+            {
+                return NotFound();
+            }
+
+            _context.ApplicationLogs.Remove(applicationLog);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ApplicationLogExists(int id)
+        {
+            return _context.ApplicationLogs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AppTrackerBE/Web/AppTracker.Web/Controllers/TrackedItemsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppTracker.Data;
+using AppTracker.Data.Models;
+
+namespace AppTracker.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackedItemsController : ControllerBase
+    {
+        private readonly AppTrackerDbContext _context;
+
+        public TrackedItemsController(AppTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TrackedItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TrackedItem>>> GetTrackedItems()
+        {
+            return await _context.TrackedItems.ToListAsync();
+        }
+
+        // GET: api/TrackedItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TrackedItem>> GetTrackedItem(int id)
+        {
+            var trackedItem = await _context.TrackedItems.FindAsync(id);
+
+            if (trackedItem == null)
+            {
+                return NotFound();
+            }
+
+            return trackedItem;
+        }
+
+        // PUT: api/TrackedItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrackedItem(int id, TrackedItem trackedItem)
+        {
+            if (id != trackedItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trackedItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrackedItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TrackedItems
+        [HttpPost]
+        public async Task<ActionResult<TrackedItem>> PostTrackedItem(TrackedItem trackedItem)
+        {
+            _context.TrackedItems.Add(trackedItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTrackedItem", new { id = trackedItem.Id }, trackedItem);
+        }
+
+        // DELETE: api/TrackedItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrackedItem(int id)
+        {
+            var trackedItem = await _context.TrackedItems.FindAsync(id);
+            if (trackedItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TrackedItems.Remove(trackedItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TrackedItemExists(int id)
+        {
+            return _context.TrackedItems.Any(e => e.Id == id);
+        }
+    }
+}
